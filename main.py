429189f6e77049f93238e3683bb6e5add61efea3
BACKUP00907 <140871090+BACKUP00907@users.noreply.github.com>
2024-01-07 06:24:32 +0000

    gjug

--- main.py
+++ main.py
@@ -11,5 +11,13 @@ def hello_world():
 def login():
    return "under construction"
 
+@app.route('/signup')
+def signup():
+   return "under construction"
+
+@app.route('/aboutus')
+def aboutus():
+   return "under construction"
+
 if __name__ == '__main__':
    app.run("0.0.0.0",port=443)
